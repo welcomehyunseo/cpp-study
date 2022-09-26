afb3f5bd77f855e21983ecb898064728642ef95e
BuildTools <welcomehyunseo@gmail.com>
2022-09-27 01:48:28 +0900

    save

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+void* operator new(size_t size) {
+    std::cout << "allocation " << size << " bytes" << std::endl;
+    return malloc(size);
+}
+
+struct Object {
+    int x, y, z;
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Object* obj  = new Object{1, 2, 3};
+
     return 0;
 }
