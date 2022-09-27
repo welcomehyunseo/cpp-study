d11b57707f870449c81aa0f34e6ddee97f3dfcd4
BuildTools <welcomehyunseo@gmail.com>
2022-09-28 04:30:35 +0900

    save

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+class Entity {
+private:
+    std::string name;
+public:
+    Entity(std::string name): name(name) {
+        std::cout << "Created entity named " << name << std::endl;
+    }
+};
+
+Entity* CreateEntityOnStack() {
+    Entity e = Entity("entity");
+    return &e;  // deallocated immediately when out of this function scope. because of stack variable.
+}
+
+Entity* CreateEntityOnHeap() {
+    Entity* e = new Entity("entity1");
+    return e;  // not deallocated until use delete keyword. because of heap variable.
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Entity* e = CreateEntityOnStack();  // NULL
+    Entity* e1 = CreateEntityOnHeap();  // 0x1037050
+    delete e1;
+
     return 0;
 }
